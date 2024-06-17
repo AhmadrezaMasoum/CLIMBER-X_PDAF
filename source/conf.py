b6ae83ef838ac94a752c39ae0a8fbf6e2082a678
Ahmadreza <77793275+AhmadrezaMasoum@users.noreply.github.com>
2024-06-17 12:03:28 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -45,7 +45,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.githubpages',
-    
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
